051859c52ee9de0f0279190d8038ee8f51529628
tomaszpankowski <--get-all>
2022-05-22 22:45:14 +0200

    layout

---
+++ src/app/books/books-sidebar/books-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-books-sidebar',
+  templateUrl: './books-sidebar.component.html',
+  styleUrls: ['./books-sidebar.component.scss']
+})
+export class BooksSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
